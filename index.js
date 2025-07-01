7f81b2d310d8c050e2edf2b8cec8684fc66b058f
cepiseptiyana <cepifams3@gmail.com>
2025-07-01 21:00:41 +0700

    feat:changePort

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = require("./src/app");
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log("running app port" + PORT);
-});
+module.exports = app;
+
+// app.listen(PORT, () => {
+//   console.log("running app port" + PORT);
+// });
