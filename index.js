92186c0526bb0b79fee08d0cca50ad26be6eb72e
cepiseptiyana <cepifams3@gmail.com>
2025-07-01 20:23:40 +0700

    switch serverless to app.listen

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 // Ambil express app dari app.js
 require("dotenv").config();
 const app = require("./src/app");
+
 const PORT = process.env.PORT || 3306;
-const serverless = require("serverless-http");
 
-module.exports = serverless(app);
-
-// app.listen(PORT, () => {
-//   console.log("running app port" + PORT);
-// });
+app.listen(PORT, () => {
+  console.log("running app port" + PORT);
+});
